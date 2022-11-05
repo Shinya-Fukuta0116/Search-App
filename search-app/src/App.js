5bf2b3b967a9037f8bb07c76a1e51b6445e20ba0
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-11-05 09:49:41 +0900

    first commit

---
+++ search-app/src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setUsers(data));
+  }, []);
+
+  console.log(users);
+
+
+  return (
+    <div className="App">
+      <div className="main">
+        <h2>Search App</h2>
+        <input type="text" />
+        <div className="content">
+          {users.map((user) => (
+            <div className="box">
+              <h3>{user.name}</h3>
+              <hr />
+              <p>{user.email}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div >
+  );
+}
+
+export default App;
+
